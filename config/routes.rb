1ade9028308a4ce1abc335f658708ef7a1ea56a9
juanan <juanansanchez_27@hotmail.com>
2017-05-25 20:54:24 +0200

    casa

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
 
 	resources :peliculas
+
+
   get 'peliculas/index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root :to => "peliculas#index"
 end
