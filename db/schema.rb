7c20d3a5a991878b40b9d3a3e705ca592ce4925d
juanan <juanansanchez_27@hotmail.com>
2017-06-05 13:17:44 +0200

    regis

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170602103839) do
+ActiveRecord::Schema.define(version: 20170605102521) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string   "namespace"
@@ -50,6 +50,15 @@ ActiveRecord::Schema.define(version: 20170602103839) do
     t.integer  "year"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "settings", force: :cascade do |t|
+    t.integer  "user_id"
+    t.string   "country"
+    t.string   "language"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_settings_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
