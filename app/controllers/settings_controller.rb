67bf5a33eab8f4588262a1e646b7333a0ae48e85
juanan <juanansanchez_27@hotmail.com>
2017-06-13 09:34:15 +0200

    cambis

--- app/controllers/settings_controller.rb
+++ app/controllers/settings_controller.rb
@@ -3,5 +3,56 @@ class SettingsController < ApplicationController
 	layout "admin"
 	
   def index
+  	@setting = current_user.setting
   end
-end
+
+  def show
+  	@setting = Setting.find(params[:id])
+  end
+
+  def new
+    @setting = current_user.build_setting
+  end  
+
+  def create
+  	@setting = current_user.build_setting(setting_params)
+
+  	if @setting.save
+  		redirect_to settings_path
+  	else
+  	 render :new
+  	end 	
+  end
+
+  def edit
+    @setting = Setting.find(params[:id])
+  end 
+
+  def update
+
+  	@setting = Setting.find(params[:id])
+ 
+
+    if @setting.update_attributes(setting_params)
+      redirect_to settings_path
+    else
+      render :index
+    end
+  end
+ 
+
+
+
+
+def destroy
+   @setting = Setting.find(params[:id])
+   @setting.destroy
+   redirect_to settings_path
+end   
+    private
+
+    def setting_params
+     params.require(:setting).permit(:language,:country)
+    end
+
+end     
