b067816155de3a1b1d8f58b92d5de0333d7b5109
juanan <juanansanchez_27@hotmail.com>
2017-06-14 14:19:34 +0200

    admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,19 @@
 Rails.application.routes.draw do
 
-  get 'settings/index'
-
-  devise_for :admin_users, ActiveAdmin::Devise.config
-  ActiveAdmin.routes(self)
   devise_for :users
 
-  root :to => "statics#index", as: 'home'
+  devise_for :admin_users, ActiveAdmin::Devise.config
 
-  
+  ActiveAdmin.routes(self)
+
+  get 'settings/index'
+
+ 
   resources :peliculas
   resources :settings
   
   get '/about' => 'statics#about', :as => :about
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root :to => "statics#index", as: 'home'
 end
